a2cd37f3e363a3746893a2e729bf65dea9340ab0
ilabstaff <ilabstaff@172.16.116.77>
2019-03-13 17:54:01 -0700

    ok

---
+++ src/PrimeorNot.java
@@ -0,0 +1,50 @@
+import javax.swing.JOptionPane;
+
+public class PrimeorNot {
+public static void main(String[] args) {
+	
+	String number = JOptionPane.showInputDialog("Number");
+	int Number = Integer.parseInt(number);
+	 for (int i = 0; i < 100; i++) {
+		
+		if(Number ) {
+		
+	
+			
+			
+			
+			
+			
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
