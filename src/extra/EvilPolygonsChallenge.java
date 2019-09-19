2e8b0a65cbc7718906904caf968986d9dab24ecc
ilabstaff <ilabstaff@172.16.116.128>
2019-09-18 17:56:53 -0700

    9/18/2019

--- src/extra/EvilPolygonsChallenge.java
+++ src/extra/EvilPolygonsChallenge.java
@@ -10,20 +10,39 @@ public class EvilPolygonsChallenge {
 	public static void main(String[] args) {
 		
 		//1. Create a new Robot
-
+		Robot Rob = new Robot();
+	Rob.penDown();
 		//2. Set the speed to 100
-
-		int colorChoice=JOptionPane.showOptionDialog(null, "hello", "title", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new String[]{"Color 1","Color 2", "Color 3"}, 0);
+Rob.setSpeed(100);
+		int colorChoice=JOptionPane.showOptionDialog(null, "hello", "title", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new String[]{"Red","Green", "Blue"}, 0);
 		
 		//3. Use if statements to check the the value of colorChoice and set the pen color accordingly 
-		
+		if(colorChoice == 0) {
+			Rob.setPenColor(Color.RED);
+		}
+		if(colorChoice == 1) {
+			Rob.setPenColor(Color.GREEN);
+		}
+		if(colorChoice == 2) {
+			Rob.setPenColor(Color.BLUE);
+		}
 		//4. Ask the use how many polygons they want to be drawn.
-		
+		String ok = JOptionPane.showInputDialog("How many polygons do you want?");
 		//5. Use the robot to draw the number of polygons the user requested.
-		
+		int okay = Integer.parseInt(ok);
 		//6. Make it so your shapes do not overlap
-
+	
 		//7. Challenge: add more colors to the Option Dialog.
+		for (int i = 0; i < okay; i++) {
+			Rob.move(100);
+			Rob.turn(90);
+			Rob.move(100);
+			Rob.turn(90);
+			Rob.move(100);
+			Rob.turn(90);
+			Rob.move(300);
+			Rob.turn(90);
+		}
 	}
 }
 
--- src/nested_loops/CircleRing.java
+++ src/nested_loops/CircleRing.java
@@ -7,29 +7,45 @@ package nested_loops;
 import org.jointheleague.graphical.robot.Robot;
 
 public class CircleRing {
-    public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception {
 
-        // 1. Make a new Robot
+		// 1. Make a new Robot
+		Robot Rob = new Robot();
 
+		// 2. Set your robot’s position to x=150 and y=200
+		Rob.setX(150);
+		Rob.setY(200);
+		// 3. Put the robot's pen down
+		Rob.penDown();
 
-       //  2. Set your robot’s position to x=150 and y=200
+		// 4. Set the robot’s speed to 10
+		Rob.setSpeed(10);
 
-        // 3. Put the robot's pen down
+		// 5. Do everything below here 360 times (use i as the counter)
+		for (int i = 0; i < 360; i++) {
 
-        // 4. Set the robot’s speed to 10
+			Rob.move(3);
+			Rob.turn(1);
+			if (i % 20 == 0) {
 
-        // 5. Do everything below here 360 times (use i as the counter)
+				for (int j = 0; j < 360; j++) {
+					Rob.move(1);
+					Rob.turn(1);
 
-	        // 6. Move the robot 3 pixels
+				}
+			}
 
-       //  7. Turn the robot 1 degree
+		}
+		// 6. Move the robot 3 pixels
 
-        // 8. If the counter i is divisible by 20 (hint: use mod operator  %)..
+		// 7. Turn the robot 1 degree
+
+		// 8. If the counter i is divisible by 20 (hint: use mod operator %)..
 
 		// 9. do steps 10 and 11 exactly 360 times (use j as the counter)
 
-				// 10. Move the robot 1 pixel
+		// 10. Move the robot 1 pixel
 
-				// 11. Turn the robot 1 degree
-}
+		// 11. Turn the robot 1 degree
+	}
 }
